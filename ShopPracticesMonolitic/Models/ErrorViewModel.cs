b897bbef493df30161e76f3d557acd548a245cd4
Deliorodriguez28@outlook.com <deliolorenzo28@gmail.com>
2024-06-11 19:23:16 -0400

    first Commit

---
+++ ShopPracticesMonolitic/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ShopPracticesMonolitic.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
